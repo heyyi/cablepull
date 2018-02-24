b173ee30aa5bd4e566dc3abc663a847d468f0033
root <yi.he@emc.com>
2018-02-24 02:52:27 -0500

    add both SLES and Redhat for environment setup

--- env.sh
+++ env.sh
@@ -1,7 +1,20 @@
 echo "nameserver 10.106.16.22" >> /etc/resolv.conf
 git clone https://github.com/axboe/fio 
-zypper install gcc
-zypper install libaio*
+grep Red /etc/*relase
+RedFlag = $?
+if [ $RedFlag == 0 ]
+then 
+   yum install gcc
+   yum install libaio*
+fi
+grep SUSE /etc/*release
+SLESFlag = $?
+if [ $SLESFlag == 0 ]
+then
+   zypper install gcc
+   zypper install libaio*
+fi
+
 cd fio
 ./configure
 make 
